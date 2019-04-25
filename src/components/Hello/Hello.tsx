cb66a4b063a86a8d422527b496d3b966feebf5ac
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2019-04-26 02:59:34 +0500

    add new Hello dummy component ✨

---
+++ src/components/Hello/Hello.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent } from 'react';
+
+interface Props {
+  name: string;
+}
+
+const Play: FunctionComponent<Props> = (props): JSX.Element => {
+  const { name } = props;
+  return (
+    <div>
+      <h1 style={{ color: '#f66e90' }}>Hello from {name}!</h1>
+    </div>
+  );
+};
+
+export default Play;
