7897fca0cd0a0a95d077875ab38d06c432c14948
Shahzaib Khalid <shahzaibkhalidmalik@outlook.com>
2019-04-26 02:57:14 +0500

    add styles.css & null checks

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
 import { setConfig } from 'react-hot-loader';
+import './styles.css';
 import App from 'components/App/App';
 
 setConfig({
@@ -8,13 +9,15 @@ setConfig({
   ignoreSFC: true,
 });
 
-const root = document.getElementById('root');
+const root: HTMLElement | null = document.getElementById('root');
 
-if (!root) throw new Error('Unable to mount your app.');
-
-render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  root,
-);
+if (root) {
+  render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+    root,
+  );
+} else {
+  throw new Error('Unable to mount your app.');
+}
